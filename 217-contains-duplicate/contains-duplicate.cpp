c49c99413decba0579dfdd235e2c9034b12bba4b
Divy Gupta <100290201+Divygupta208@users.noreply.github.com>
2023-08-26 11:16:43 +0530

    Time: 86 ms (94.26%) | Memory: 69.6 MB (49.39%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,int> map;
+
+        for(int i = 0 ;i< nums.size() ; i++){
+          
+          if(map.find(nums[i])!= map.end()){
+              return true;
+          }else{
+            map.insert({nums[i] , i});
+          }
+        
+        }
+        return false;
+    }
+};
