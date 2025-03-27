c33551232c2c4aef9b1c98051804d703a2e77104
Divy Gupta <100290201+Divygupta208@users.noreply.github.com>
2025-03-27 17:24:14 +0530

    Time: 0 ms (100%), Space: 23.9 MB (17.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      
+      int i = 0;
+      int j = 0;
+      int n = nums.size();
+
+      while(j<n){
+         
+         if(nums[j] == 0){
+            j++;
+         }else{
+            swap(nums[j] , nums[i]);
+            j++;
+            i++;
+         }
+
+      }
+              
+    }
+};
