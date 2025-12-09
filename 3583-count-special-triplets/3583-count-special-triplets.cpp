0c1848d96fa8386dde0ef6cd3ae880fb262cfaf2
Divy Gupta <100290201+Divygupta208@users.noreply.github.com>
2025-12-09 12:55:09 +0530

    Time: 599 ms (68.98%), Space: 310.1 MB (91.24%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+
+    int specialTriplets(vector<int>& nums) {
+       const int MOD = 1000000007;
+        const int MAXV = 100005;
+        vector<long long> left(MAXV, 0), right(MAXV, 0);
+        
+       
+        for (int x : nums) ++right[x];
+        
+        long long ans = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            int x = nums[j];
+            --right[x];                     
+            
+            int doubleX = x * 2;
+            if (doubleX < MAXV) {
+                ans = (ans + left[doubleX] * right[doubleX]) % MOD;
+            }
+            
+            ++left[x];                      // now it is on the left side
+        }
+        return ans;
+    }
+};
