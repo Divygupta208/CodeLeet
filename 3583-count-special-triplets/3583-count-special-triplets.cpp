ea7b39d561fe421e7de1fb22e3d1f11db954e9b9
Divy Gupta <100290201+Divygupta208@users.noreply.github.com>
2025-12-09 13:04:07 +0530

    Time: 486 ms (89.78%), Space: 365.8 MB (69.34%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,30 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-
-
     int specialTriplets(vector<int>& nums) {
-       const int MOD = 1000000007;
-        const int MAXV = 100005;
-        vector<long long> left(MAXV, 0), right(MAXV, 0);
-        
-       
-        for (int x : nums) ++right[x];
-        
-        long long ans = 0;
-        for (int j = 0; j < nums.size(); ++j) {
-            int x = nums[j];
-            right[x]--;                     
-            
-            int doubleX = x * 2;
-            if (doubleX < MAXV) {
-                ans = (ans + left[doubleX] * right[doubleX]) % MOD;
-            }
-            
-            left[x]++;                     
+        const int MOD = 1e9 + 7;
+        unordered_map<int, vector<int>> pos;
+        pos.reserve(nums.size() * 2);
+
+        for (int i = 0; i < nums.size(); i++) {
+            pos[nums[i]].push_back(i);
         }
+
+        int ans = 0;
+
+        for (int i = 1; i < nums.size() - 1; i++) {
+            int target = nums[i] * 2LL;
+            if (pos.count(target) == 0) {
+                continue;
+            }
+
+            const auto& arr = pos[target];
+            if (arr.size() <= 1 || arr[0] >= i) {
+                continue;
+            }
+
+            auto split = upper_bound(arr.begin(), arr.end(), i);
+            int l = split - arr.begin();
+            int r = arr.size() - l;
+
+            if (nums[i] == 0) {
+                l--;
+            }
+            ans = (ans + l * 1LL * r % MOD) % MOD;
+        }
+
         return ans;
     }
 };
